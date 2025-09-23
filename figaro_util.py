aaa6ea8d5e249868a6c67c2962e5d967f3f8c55a
Yuchen He <yuchen@Yuchens-MacBook-Pro.local>
2025-09-23 18:52:14 +0200

    more progress

--- figaro_util.py
+++ figaro_util.py
@@ -107,22 +107,25 @@ def find_key(S1: pd.DataFrame, S2: pd.DataFrame, impose_join_key=False):
     Black-box function to find join key(s) between two DataFrames.
     Replace this with your own logic.
     """
-    common = list(set(S1.columns) & set(S2.columns))
+    common = sorted(list(set(S1.columns) & set(S2.columns)))
 
     if not common and impose_join_key:
         raise ValueError("No common key found between tables")
     return common
 
 
-def construct_row_index(index, row, join_key_columns):
+def construct_row_index_dict(index, row, join_key_columns):
     # don't add index for now
-    return str(dict(sorted(row[join_key_columns].to_dict().items())))
+    return dict(sorted(row[join_key_columns].to_dict().items()))
 
 def Head(values, vector=None):
     if not vector:
         return f"H({', '.join(values)})"
     else:
         return f"H({', '.join(values)}, {vector})"
+
+def multiply(coefficient, variable):
+    return f"{coefficient}*{variable}"
 
 
 def down_result(index, table_access, tree_access, key_value_map):
